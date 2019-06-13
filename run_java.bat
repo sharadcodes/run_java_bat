167eff288f4c8702ccf2eb96a00ab350963f9451
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2019-06-13 15:35:59 +0530

    Create run_java.bat

---
+++ run_java.bat
@@ -0,0 +1,26 @@
+REM JAVA COMPILATION BATCH FILE 
+REM Made by Sharad Raj
+REM Author's Github: https://www.github.com/sharadcodes
+cls
+@ECHO OFF
+ECHO. Source file name: %~n1
+ECHO. Source path     : %~f1
+
+if %~x1%==.java (
+ECHO. JAVA COMPILATION IS GOING ON ------------------------------------------
+
+javac "%~f1"
+
+ECHO.
+if exist "%~dp1%~n1.class"  ECHO. ############################ OUTPUT ##############################
+ECHO.
+ECHO.
+
+if exist "%~dp1%~n1.class" (
+cd %~dp1
+java %~n1
+REM del "%~dp1%~n1.class"
+)
+ECHO.
+)
+pause
